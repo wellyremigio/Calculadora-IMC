3971f77a85e09b29530749244cac451918841a9b
Welly Remígio <85763844+wellyremigio@users.noreply.github.com>
2024-04-23 15:32:34 -0300

    Add files via upload

---
+++ CalculadoraIMC/assets/js/script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();  
+
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    const imc = (weight / (height*height)).toFixed(2);
+    const value = document.getElementById('value');
+
+    let description = '';
+
+    value.classList.add('attention');
+
+    document.getElementById('infos').classList.remove('hidden');
+
+    if(imc < 18.5){
+        description = 'Cuidado! Você está abaixo do peso!';
+    }else if(imc >= 18.5 && imc <= 24.9){
+        description = 'Parabéns! Você está no peso ideal!';
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    }else if(imc >= 25 && imc <= 29.9){
+        description = 'Atenção! Você está com sobrepeso!';
+    }else if(imc >=30 && imc <= 34.9){
+        description = 'Cuidado! Você está com obesidade moderada!';
+    }else if(imc >=35 && imc <= 39.9){
+        description = 'Cuidado! Você está com obesidade severa!'
+    }else{
+        description = 'Muito cuidado! Você está com obesidade morbida!'
+    }
+
+    value.textContent = imc.replace('.', ',');
+    document.getElementById('description').textContent = description;
+
+})
